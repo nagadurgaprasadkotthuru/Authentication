b007d3b064b7c2e96b4ee8ecc2ee687c2cd1067f
nagadurgaprasadkotthuru <nagadurgaprasadkotthuru2001@gmail.com>
2023-02-27 16:07:17 +0000

    first_commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const bcrypt = require("bcrypt");
+
+const dbPath = path.join(__dirname, "userData.db");
+const app = express();
+app.use(express.json());
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+//User Register API 1
+app.post("/register", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const getUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const existingUser = await db.get(getUserQuery);
+  if (existingUser === undefined) {
+    if (username.length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const addUserQuery = `
+                INSERT INTO
+                user(
+                    username,
+                    name,
+                    password,
+                    gender,
+                    location
+                )VALUES(
+                    '${username}',
+                    '${name}',
+                    '${hashedPassword}',
+                    '${gender}',
+                    '${location}'
+                );`;
+      const dbResponse = await db.run(addUserQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+//User Login API 2
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const getUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const existingUser = await db.get(getUserQuery);
+  if (existingUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      response.status(200);
+      response.send("Login success!");
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+//Change Password API 3
+app.put(`/change-password`, async (request, response) => {});
